5ed6120d2f8ae16787b64f4336691b3198ccaa4e
DMITRIY GRIN <greendmitriy407@gmail.com>
2024-04-30 20:44:12 +0300

    add RegisterPopup

---
+++ Assets/_App/Scripts/Lobby/RegisterPopup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Doozy.Engine.UI;
+using TMPro;
+using UnityEngine;
+
+namespace _App.Scripts.Lobby
+{
+	internal class RegisterPopup : MonoBehaviour
+	{
+		[SerializeField] private UIPopup _instance;
+		[SerializeField] private TMP_InputField _nameInput;
+		[SerializeField] private UIButton _createUserButton;
+
+		public event Action<RegisterPopup, string> OnUserNameRegistered;
+
+		private void Start()
+		{
+			_createUserButton.Interactable = false;
+			_nameInput.onValueChanged.AddListener(OnChangeValue);
+			_createUserButton.OnClick.OnTrigger.Action = CreateUserButtonPressed;
+		}
+
+		private void OnChangeValue(string arg0)
+		{
+			if (arg0.Length < 4 || arg0.Length > 9)
+			{
+				_createUserButton.Interactable = false;
+			}
+			else
+			{
+				_createUserButton.Interactable = true;
+			}
+		}
+
+		private void CreateUserButtonPressed(GameObject obj)
+		{
+			_instance.Hide();
+			OnUserNameRegistered?.Invoke(this, _nameInput.text);
+		}
+	}
+}
