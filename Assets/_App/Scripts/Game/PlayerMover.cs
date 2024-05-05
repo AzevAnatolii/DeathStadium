ea112956d8fcda54aad7674c94d802842d856cdd
DMITRIY GRIN <greendmitriy407@gmail.com>
2024-05-05 05:47:54 +0300

    player movement

--- Assets/_App/Scripts/Game/PlayerMover.cs
+++ Assets/_App/Scripts/Game/PlayerMover.cs
@@ -1,8 +1,27 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMover : NetworkBehaviour
 {
+    [SerializeField] private float _speed;
+    private PlayerInput _input;
+
+    private void Start()
+    {
+        _input = new PlayerInput();
+        _input.Enable();
+    }
+
+    private void Update()
+    {
+        if (_input.Input.WASD.IsPressed())
+        {
+            Vector2 vector = _input.Input.WASD.ReadValue<Vector2>();
+            transform.position += new Vector3(vector.x * _speed, 0, vector.y * _speed);
+        }
+    }
+
     public override void OnStartAuthority()
     {
         enabled = true;
